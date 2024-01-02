989b00508658620b15aad49ae2a0a61bc72ab2dd
Damien Ostler <damienostler1@outlook.com>
2024-01-01 21:24:39 -0500

    Delete scripts/mousetest.gd

--- scripts/mousetest.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-@export var tilemap : TileMap # Make sure to assign your TileMap node in the Inspector
-
-func _process(_delta: float) -> void:
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
-		var mouse_position : Vector2 = get_global_mouse_position()
-		var cell = tilemap.get_cell(mouse_position.x, mouse_position.y)
-		print(cell.get_name())
